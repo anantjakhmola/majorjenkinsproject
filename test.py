1bbefe636d5749ba67749f1e9999b2deb17745ab
anant jakhmola <anantjakhmola9@mail.com>
2020-05-16 17:50:38 +0530

    edit 2

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ for line in file:
     if 'Sequential' in line:
         print('sequential')
         os.system("sudo cp /Email/newmail.py /ajproject")
-        subprocess.Popen("sudo docker run -it -v /ajproject:/root --name myrpd anantj1/rcnn:v1",shell=True)
-        subprocess.Popen("sudo docker exec -d -w /root myrpd -c 'python3 newmail.py'",shell=True)
+        os.system("sudo docker run -it -v /ajproject:/root --name myrpd anantj1/rcnn:v1")
+        os.system("sudo docker exec -d -w /root myrpd -c 'python3 newmail.py'")
         #subprocess.Popen("hello.py 1", shell=True)
         #subprocess.Popen("newmail.py 1",shell=True)
         break
